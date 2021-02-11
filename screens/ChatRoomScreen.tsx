b0366b01052ab95ce3b0317c8033511a42269fb9
Douglas-Waynes <theetechverge@gmail.com>
2021-02-11 14:33:36 +0300

    Added the onCLick function to chat components as well as their chat rooms

---
+++ screens/ChatRoomScreen.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Text } from "react-native";
+
+import { useRoute } from "@react-navigation/native";
+
+const ChatRoomScreen = () => {
+	const route = useRoute();
+
+	//console.log(route.params);
+
+	return <Text>Chat room</Text>;
+};
+
+export default ChatRoomScreen;
